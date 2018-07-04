07e9c50c634a1a4a28c50d3b98617e62e08e7d8e
Babar, Ajinkya <ajinkya_babar_tp@bmc.com>
2018-07-04 12:17:52 +0530

    removed test stage

--- testGroovy.groovy
+++ testGroovy.groovy
@@ -10,11 +10,4 @@ node() {
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
 		])		
 	}
-
-	stage('test') {
-		sh "echo 'Throw in some tests here'"
-		sh "echo 'Throw in some tests here'"
-		sh "echo 'Throw in new tests here'"
-
-	}
 }
