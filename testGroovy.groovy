68dda4ad80e2537dd95f680af7616068726af2f0
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-07-28 23:50:44 +0530

    Update testGroovy.groovy

--- testGroovy.groovy
+++ testGroovy.groovy
@@ -5,16 +5,11 @@ node() {
      input 'Do you want to continue?'	
 		checkout ([
 		$class: 'GitSCM',
-		branches: [[name: '*/master']],
+			branches: [[name: '${BRANCH}']],
 		extensions: [				
 				[$class: 'CleanCheckout']
 		],
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
 		])		
 	}
-    stage ('Build') {
-           git 'https://github.com/ajinkyababar/hello_world1.git'
-           step([$class: 'LastChangesPublisher', since:'PREVIOUS_REVISION',specificRevision: '', format: 'LINE', matchWordsThreshold: '0.25', matching: 'NONE', matchingMaxComparisons: '1000', showFiles: true, synchronisedScroll: true])
-
-      }
 }
