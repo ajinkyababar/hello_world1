03a78ee1ca4f3b3eea39c7fca5744436774ca6ec
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-07-28 23:55:40 +0530

    Update testGroovy.groovy

--- testGroovy.groovy
+++ testGroovy.groovy
@@ -2,7 +2,8 @@
 node() {
 
 	stage('checkout ') {	
-     input 'Do you want to continue?'	
+     input 'Do you want to continue?'
+		echo "${BRANCH}"
 		checkout ([
 		$class: 'GitSCM',
 			branches: [[name: '${BRANCH}']],
