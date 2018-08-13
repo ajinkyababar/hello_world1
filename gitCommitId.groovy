7846a63ba65993e4f139f7f6f3688f31450d0e18
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-08-13 18:01:23 +0530

    Create gitCommitId.groovy

---
+++ gitCommitId.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+node() {
+
+	stage('checkout from git-2 ') {		
+		checkout ([
+		$class: 'GitSCM',
+		branches: [[name: '*/master']],
+		extensions: [				
+				[$class: 'CleanCheckout']
+		],
+		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
+		])		
+	}
+}
