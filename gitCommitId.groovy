3d3b1f2a6b534b8b74e0c058d382bd8fe9ba4659
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-08-13 18:28:44 +0530

    Update gitCommitId.groovy

--- gitCommitId.groovy
+++ gitCommitId.groovy
@@ -2,6 +2,7 @@
 node() {
 
 	def branch = env.BRANCH_NAME
+	
 	stage('checkout from git-2 ') {		
 		checkout ([
 		$class: 'GitSCM',
@@ -10,8 +11,12 @@ node() {
 				[$class: 'CleanCheckout']
 		],
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
-		])		
+		])
+		env.GIT_COMMIT = scmVars.GIT_COMMIT
 	}
 	
-	echo " branch"
+	echo "env.GIT_COMMIT"
+	echo "branch"
+	sh 'echo $BRANCH_NAME'
+	git ls-remote
 }
