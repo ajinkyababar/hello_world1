3e75ce9252d11b52b4455b9f44dcc0de6a4696da
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-08-13 19:02:10 +0530

    Update gitCommitId.groovy

--- gitCommitId.groovy
+++ gitCommitId.groovy
@@ -13,8 +13,9 @@ node() {
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
 		])
 		GIT_COMMIT = scmVars.GIT_COMMIT
+		env.GIT_COMMIT = scmVars.GIT_COMMIT
 	}
-	print GIT_COMMIT
+	println("First commit Id = "+GIT_COMMIT)
 	
 	def scmVars2 = checkout ([
 		$class: 'GitSCM',
@@ -25,6 +26,9 @@ node() {
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/new-git-project.git']]
 		])
 		GIT_COMMIT2 = scmVars2.GIT_COMMIT
-	print GIT_COMMIT2
+		env.GIT_COMMIT2 = scmVars2.GIT_COMMIT
+		print GIT_COMMIT2
+		
+	println("Final result = "+env)
       
 }
