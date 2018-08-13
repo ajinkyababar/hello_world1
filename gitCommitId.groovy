685b3c71b7bb6d9315b90730482129f79bd7d182
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-08-13 18:55:22 +0530

    Update gitCommitId.groovy

--- gitCommitId.groovy
+++ gitCommitId.groovy
@@ -15,5 +15,16 @@ node() {
 		GIT_COMMIT = scmVars.GIT_COMMIT
 	}
 	print GIT_COMMIT
+	
+	def scmVars2 = checkout ([
+		$class: 'GitSCM',
+		branches: [[name: '*/master']],
+		extensions: [				
+				[$class: 'CleanCheckout']
+		],
+		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/new-git-project.git']]
+		])
+		GIT_COMMIT2 = scmVars2.GIT_COMMIT
+	print GIT_COMMIT2
       
 }
