b7f1a40fb3632258227b866f1b408824550bfa6b
Ajinkya Babar <ajinkyababar93@gmail.com>
2018-08-14 16:02:31 +0530

    Update testGroovy.groovy

--- testGroovy.groovy
+++ testGroovy.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 node() {
 
+	 def BSAConnectorBuildJob = 'bsaBuild'
+	 def BSAConnectorPipelineBuildNumber = "${env.BUILD_NUMBER}"
 	stage('checkout from gitHub') {		
 		checkout ([
 		$class: 'GitSCM',
@@ -11,4 +13,14 @@ node() {
 		userRemoteConfigs: [[url: 'https://github.com/ajinkyababar/hello_world1.git']]
 		])		
 	}
+	
+	
+	stage('Build Codebase') {
+    build_bsa_connector = build job: "${BSAConnectorBuildJob}",
+    parameters: [
+        string(name: 'CONNECTOR_BUILD_NUMBER', value: "${BSAConnectorPipelineBuildNumber}"),
+	     string(name: 'GitBranch', value: "${GitBranch}")
+    ]
+    
+  }
 }
